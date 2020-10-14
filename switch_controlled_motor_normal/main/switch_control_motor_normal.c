e08e96785e002926d5f6b5e81fe8ea380c4f50b2
luke2103 <lnankamwar_b18@el.vjti.ac.in>
2020-10-14 23:42:00 +0530

    Reslved issue in led_blink and switch_control

--- switch_controlled_motor_normal/main/switch_control_motor_normal.c
+++ switch_controlled_motor_normal/main/switch_control_motor_normal.c
@@ -4,10 +4,8 @@ static const char* TAG_SWITCH_CONTROL_MOTOR = "switch_control";
 
 void drive_task(void *arg)
 {
-
 	enable_switches();
 	enable_motor_driver(a, NORMAL_MODE); // Enable motor driver A in Normal Mode
-	
 	while(1)
 	{
 		if(read_switch(SWITCH_1))
@@ -15,7 +13,6 @@ void drive_task(void *arg)
 			set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, 80);
 			ESP_LOGI(TAG_SWITCH_CONTROL_MOTOR, "MOTOR_A_0 FORWARD");	
 		}
-		
 		else if(read_switch(SWITCH_2))
 		{
 			set_motor_speed(MOTOR_A_0, MOTOR_BACKWARD, 80);
@@ -24,7 +21,6 @@ void drive_task(void *arg)
 		else
 		{
 			set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
-			
 		}
 
 		if(read_switch(SWITCH_3))
@@ -40,21 +36,15 @@ void drive_task(void *arg)
 		else
 		{
 			set_motor_speed(MOTOR_A_1, MOTOR_STOP, 0);
-			
-	
 		}
 			
 	}
-	
-	
 }
-
-
 void app_main()
 {
-	
-    
-        xTaskCreate(&drive_task,"drive_task",4096,NULL,1,NULL);
-    
-   
+	/**
+	 * @brief Basic Function for task creation
+	 * 
+	 */
+	xTaskCreate(&drive_task,"drive_task",4096,NULL,1,NULL);
 }

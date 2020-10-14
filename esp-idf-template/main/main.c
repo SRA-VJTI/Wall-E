98eb31279d3baa2062c509e9fd2731b43b8b78e5
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-10-14 14:46:00 +0530

    Removed FastPID submodule

---
+++ esp-idf-template/main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "sra_board.h"
+
+void app_main(void)
+{
+    int i = 0;
+    while (1) {
+        printf("[%d] Hello world!\n", i);
+        i++;
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}

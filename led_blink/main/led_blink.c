ca76331c9d9b350031edeb9fd9338729112b1be8
luke2103 <lnankamwar_b18@el.vjti.ac.in>
2020-10-14 20:10:45 +0530

    switch_control and led_blink

---
+++ led_blink/main/led_blink.c
@@ -0,0 +1,30 @@
+//C Headers
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+//Components
+#include "sra_board.h"
+
+void blink_task(void *arg)
+{
+	enable_bar_graph();
+	while(1)
+	{
+		
+		set_bar_graph(0xFF);
+		vTaskDelay(100 / 10);
+		set_bar_graph(0x00);
+	}
+}
+
+void app_main()
+{
+	/*
+		Basic Function for task creation
+	*/
+
+	xTaskCreate(&blink_task,"blink task",4096,NULL,1,NULL);
+}

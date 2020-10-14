c589f69fd5fd2db47c796d1cff0bc65a006f964c
luke2103 <lnankamwar_b18@el.vjti.ac.in>
2020-10-14 20:10:45 +0530

    switch_control and led_blink

---
+++ switch_controlled_motor_normal/main/switch_control_motor_normal.c
@@ -0,0 +1,56 @@
+#include "sra_board.h"
+
+static const char* TAG_SWITCH_CONTROL_MOTOR = "switch_control";
+
+void drive_task(void *arg)
+{
+
+	enable_switches();
+	enable_motor_driver_a(NORMAL_MODE); // Enable motor driver A in Normal Mode
+	
+	while(1)
+	{
+		if(read_switch(SWITCH_1))
+		{
+			set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, 80);
+			ESP_LOGI(TAG_SWITCH_CONTROL_MOTOR, "MOTOR_A_0 FORWARD");	
+		}
+		
+		else if(read_switch(SWITCH_2))
+		{
+			set_motor_speed(MOTOR_A_0, MOTOR_BACKWARD, 80);
+			ESP_LOGI(TAG_SWITCH_CONTROL_MOTOR, "MOTOR_A_1 BACKWARD");
+		}
+		else if(read_switch(SWITCH_3))
+		{
+			set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, 80);
+			ESP_LOGI(TAG_SWITCH_CONTROL_MOTOR, "MOTOR_B_0 FORWARD");
+		}
+		else if(read_switch(SWITCH_4))
+		{
+			set_motor_speed(MOTOR_A_1, MOTOR_BACKWARD, 80);
+			ESP_LOGI(TAG_SWITCH_CONTROL_MOTOR, "MOTOR_B_1 BACKWARD");
+		}
+		else
+		{
+			set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
+			set_motor_speed(MOTOR_A_1, MOTOR_STOP, 0);
+	
+		}
+			
+	}
+	
+	
+}
+
+
+void app_main()
+{
+	/*
+		Basic Function for task creation
+	*/
+    
+        xTaskCreate(&drive_task,"drive_task",4096,NULL,1,NULL);
+    
+   
+}

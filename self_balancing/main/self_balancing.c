1b14b156ea9ef62b5eac81071ea512db2e664ec6
OSSome01 <ossargar_b18@el.vjti.ac.in>
2020-10-25 14:45:57 +0530

    remove global variables

--- self_balancing/main/self_balancing.c
+++ self_balancing/main/self_balancing.c
@@ -8,11 +8,11 @@
 #include <time.h>
 
 //Limiting Variables
-#define MAX_PITCH_CORRECTION 90
-#define MAX_PITCH_CUMULATIVE_ERROR 850
+#define MAX_PITCH_CORRECTION (90)
+#define MAX_PITCH_CUMULATIVE_ERROR (850)
 
-#define MAX_PWM 100
-#define MIN_PWM 60
+#define MAX_PWM (100)
+#define MIN_PWM (60)
 
 //Self Balancing Tuning Parameters
 float pitch_kP = 15.1f;	
@@ -26,12 +26,10 @@ float forward_angle = 0.0f;
 float forward_offset = 2.51f;
 float forward_buffer = 3.1f;
 
-//Error and correction values
-float absolute_pitch_correction = 0.0f, absolute_pitch_angle = 0.0f, pitch_angle = 0.0f, roll_angle = 0.0f, pitch_error = 0.0f, prevpitch_error = 0.0f, pitchDifference = 0.0f, pitch_cumulative_error = 0.0f, pitch_correction = 0.0f, integral_term = 0.0f;
-
+float pitch_angle = 0.0f, roll_angle = 0.0f;
 float motor_pwm = 0.0f;
 
-int constrain(int val, int lower_limit, int higher_limit)
+int bound(int val, int lower_limit, int higher_limit)
 {
 	if (val < lower_limit)
 	{
@@ -46,26 +44,30 @@ int constrain(int val, int lower_limit, int higher_limit)
 }
 
 // Calculate the motor inputs according to angle of the MPU
-void calculate_pitch_error()
+void calculate_motor_command(const float pitch_cmd, const float pitch_angle, float *motor_cmd)
 {
-	pitch_error = pitch_angle;
-	pitchDifference = (pitch_error - prevpitch_error);
-	pitch_cumulative_error += pitch_error;
+	static float pitch_error = 0.0f;
+	static float pitch_error_difference = 0.0f;
+    static float pitch_error_cummulative = 0.0f;
 
-	if (pitch_cumulative_error > MAX_PITCH_CUMULATIVE_ERROR)
-	{
-		pitch_cumulative_error = MAX_PITCH_CUMULATIVE_ERROR;
-	}
-	else if (pitch_cumulative_error < -MAX_PITCH_CUMULATIVE_ERROR)
-	{
-		pitch_cumulative_error = -MAX_PITCH_CUMULATIVE_ERROR;
-	}
+	static float prevpitch_error = 0.0f;
 
-	pitch_correction = pitch_kP * pitch_error + pitch_kI * pitch_cumulative_error + pitch_kD * pitchDifference;
-	prevpitch_error = pitch_error;
+    static float pitch_correction = 0.0f;
+    static float absolute_pitch_correction = 0.0f;
+
+	pitch_error = pitch_cmd - pitch_angle;
+	pitch_error_difference = pitch_error - prevpitch_error;
+	pitch_error_cummulative += pitch_error;
+
+	// integral term bounding, prevent windup
+	pitch_error_cummulative = bound(pitch_error_cummulative, -MAX_PITCH_CUMULATIVE_ERROR, MAX_PITCH_CUMULATIVE_ERROR)
+
+	pitch_correction = pitch_kP * pitch_error + pitch_kI * pitch_error_cummulative + pitch_kD * pitch_error_difference;
 
 	absolute_pitch_correction = fabs(pitch_correction);
-	absolute_pitch_correction = constrain(absolute_pitch_correction, 0, MAX_PITCH_CORRECTION);
+	*motor_cmd = bound(absolute_pitch_correction, 0, MAX_PITCH_CORRECTION);
+
+	prevpitch_error = pitch_error;
 }
 
 //The main task to balance the robot
@@ -83,12 +85,15 @@ void balance_task(void *arg)
 			{
 
 				pitch_angle = euler_angle[1];
-				calculate_pitch_error();
+				
+				float pitch_cmd = 0.0f;
+				float motor_cmd = 0.0f;
+				calculate_motor_command(pitch_cmd, pitch_angle, &motor_cmd);
+
+				//bound PWM values between max and min
+				motor_pwm = bound((motor_cmd), MIN_PWM, MAX_PWM);
+
 				enable_motor_driver(a, NORMAL_MODE);
-
-				//constrain PWM values between max and min
-				motor_pwm = constrain((absolute_pitch_correction), MIN_PWM, MAX_PWM);
-
 				if (pitch_error > 1)
 				{
 					set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, motor_pwm);

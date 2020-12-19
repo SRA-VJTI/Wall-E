e6930d9a62ba2f7bff4dacf9edbc70139682d14c
laukik-hase <laukik17@gmail.com>
2020-12-19 17:54:19 +0530

    Modified MPU test code

--- mpu_test/main/main.c
+++ mpu_test/main/main.c
@@ -14,17 +14,16 @@ static const char *TAG = "mpu_test";
 
 void mpu_task(void *arg)
 {
+    float euler_angle[2], mpu_offset[2] = {0.0f, 0.0f};
+
     while (1)
     {
-        float euler_angle[2];
-        if (i2c_master_init() == ESP_OK && enable_mpu6050() == ESP_OK)
+        if (enable_mpu6050() == ESP_OK)
         {
-            while (1)
-            {
-                if (read_mpu6050(euler_angle) == ESP_OK)
-                    ESP_LOGD(TAG, "Roll: %0.2f Pitch: %0.2f", euler_angle[0], euler_angle[1]);
-            }
+            while (read_mpu6050(euler_angle, mpu_offset) == ESP_OK)
+                ESP_LOGI(TAG, "Roll: %0.2f | Pitch: %0.2f", euler_angle[0], euler_angle[1]);
         }
+        
         ESP_LOGE(TAG, "MPU Initialisation Failed / Connection Broke!");
     }
 }

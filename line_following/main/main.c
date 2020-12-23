823191391b9861f5848972e5463f58a47d9a479e
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-12-24 02:05:15 +0530

    Added http rest server for tuning pid parameters

--- line_following/main/main.c
+++ line_following/main/main.c
@@ -2,18 +2,13 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sra_board.h"
+#include "tuning_http_server.h"
 
 #define MODE NORMAL_MODE
 #define BLACK_MARGIN 400
 #define WHITE_MARGIN 2000
-#define CONSTRAIN_LSA_LOW 0
-#define CONSTRAIN_LSA_HIGH 1000
-/*
- * Line Following PID Constants
- */
-#define kP 1.3
-#define kI 0
-#define kD 1.9
+#define bound_LSA_LOW 0
+#define bound_LSA_HIGH 1000
 
 /*
  * weights given to respective line sensor
@@ -21,7 +16,7 @@
 const int weights[4] = {1,3,-3,-1};
 
 /*
- * Motor value constraints
+ * Motor value boundts
  */
 int optimum_duty_cycle = 70;
 int lower_duty_cycle = 60;
@@ -38,30 +33,17 @@ float error=0, prev_error=0, difference, cumulative_error, correction;
  */
 line_sensor_array line_sensor_readings;
 
-// #########################################################################
-
-int constrain(int val, int lower_limit, int higher_limit)
-{
-    return val < lower_limit ? lower_limit : (val > higher_limit ?  higher_limit : val);
-}
-
-int map(int val, int input_lower_limit, int input_higher_limit, int output_lower_limit, int output_higher_limit)
-{
-    return (output_lower_limit + ((float)(val-input_lower_limit)*((float)(output_higher_limit - output_lower_limit)/(float)(input_higher_limit - input_lower_limit))));
-}
-
 void calculate_correction()
 {
     error = error*10;  // we need the error correction in range 0-100 so that we can send it directly as duty cycle paramete
     difference = error - prev_error;
     cumulative_error += error;
 
-    cumulative_error = constrain(cumulative_error, -30, 30);
+    cumulative_error = bound(cumulative_error, -30, 30);
 
-    correction = kP*error + kI*cumulative_error + kD*difference;
+    correction = read_pid_const().kp*error + read_pid_const().ki*cumulative_error + read_pid_const().kd*difference;
     prev_error = error;
 }
-
 
 void calculate_error()
 {
@@ -101,7 +83,6 @@ void calculate_error()
     }
 }
 
-
 void line_follow_task(void* arg)
 {
     ESP_ERROR_CHECK(enable_motor_driver(a, NORMAL_MODE));
@@ -112,24 +93,27 @@ void line_follow_task(void* arg)
         line_sensor_readings = read_line_sensor();
         for(int i = 0; i < 4; i++)
         {
-            line_sensor_readings.adc_reading[i] = constrain(line_sensor_readings.adc_reading[i], BLACK_MARGIN, WHITE_MARGIN);
-            line_sensor_readings.adc_reading[i] = map(line_sensor_readings.adc_reading[i], BLACK_MARGIN, WHITE_MARGIN, CONSTRAIN_LSA_LOW, CONSTRAIN_LSA_HIGH);
+            line_sensor_readings.adc_reading[i] = bound(line_sensor_readings.adc_reading[i], BLACK_MARGIN, WHITE_MARGIN);
+            line_sensor_readings.adc_reading[i] = map(line_sensor_readings.adc_reading[i], BLACK_MARGIN, WHITE_MARGIN, bound_LSA_LOW, bound_LSA_HIGH);
         }
         
         calculate_error();
         calculate_correction();
 
-        left_duty_cycle = constrain((optimum_duty_cycle - correction), lower_duty_cycle, higher_duty_cycle);
-        right_duty_cycle = constrain((optimum_duty_cycle + correction), lower_duty_cycle, higher_duty_cycle);
+        left_duty_cycle = bound((optimum_duty_cycle - correction), lower_duty_cycle, higher_duty_cycle);
+        right_duty_cycle = bound((optimum_duty_cycle + correction), lower_duty_cycle, higher_duty_cycle);
 
         set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, left_duty_cycle);
         set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, right_duty_cycle);
+
+        vTaskDelay(1 / portTICK_PERIOD_MS);
     }
+
+    vTaskDelete(NULL);
 }
-
-
 
 void app_main()
 {
-    xTaskCreate(&line_follow_task, "line_follow_task", 100000, NULL, 1, NULL);
+    start_tuning_http_server();
+    xTaskCreate(&line_follow_task, "line_follow_task", 4096, NULL, 1, NULL);
 }

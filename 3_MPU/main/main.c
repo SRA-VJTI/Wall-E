4b08428eabe5a59223d602d1f0ec89386a2642ec
MOLOCH-dev <sabnisanushree@gmail.com>
2021-02-10 02:16:23 +0530

    Added comments to 3_MPU

--- 3_MPU/main/main.c
+++ 3_MPU/main/main.c
@@ -13,22 +13,30 @@
 static const char *TAG = "mpu_test";
 
 void mpu_task(void *arg)
-{
+{   //Euler angle buffer includes two floats - Roll angle and Pitch angle
     float euler_angle[2], mpu_offset[2] = {0.0f, 0.0f};
 
     while (1)
     {
-        if (enable_mpu6050() == ESP_OK)
+        /*The MPU6050 comes up in sleep mode upon power-up
+        enable_mpu6050 disables the SLEEP_MODE of the MPU by accessing the POWER_MANAGEMENT_1 register
+         */
+        if (enable_mpu6050() == ESP_OK) //Checks if MPU is initialised correctly
         {
+            /* Euler angles are obtained by reading values from the accelerometer and gyroscope
+            Calculating roll, pitch from raw accelerometer and gyroscope readings seperately
+            And then performing sensor fusion via complementary filters to obtain a stable reading
+            with low pass filter on accelerometer data and high pass filter on gyroscope data
+            */
             while (read_mpu6050(euler_angle, mpu_offset) == ESP_OK)
-                ESP_LOGI(TAG, "Roll: %0.2f | Pitch: %0.2f", euler_angle[0], euler_angle[1]);
+                ESP_LOGI(TAG, "Roll: %0.2f | Pitch: %0.2f", euler_angle[0], euler_angle[1]); //Logging information of roll and pitch angles
         }
         
-        ESP_LOGE(TAG, "MPU Initialisation Failed / Connection Broke!");
+        ESP_LOGE(TAG, "MPU Initialisation Failed / Connection Broke!"); //Logging Error
     }
 }
 
 void app_main()
 {
-    xTaskCreate(mpu_task, "mpu_task", 4096, NULL, 1, NULL);
+    xTaskCreate(mpu_task, "mpu_task", 4096, NULL, 1, NULL); //creating task to run mpu_task function 
 }

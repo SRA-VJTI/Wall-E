cccdba5bc5370a4ae26211ff313c174ab44b995d
Purvank Bhiwgade <59529780+purvankbhiwgade@users.noreply.github.com>
2021-02-09 17:00:54 +0530

    Added more technical terms

--- 1_led_blink/main/main.c
+++ 1_led_blink/main/main.c
@@ -20,14 +20,19 @@ void app_main()
         uint8_t var = 0x01;
 
         while(1)
-        {
+        {   
+            // After left-shifting 0x01 8-times, we end up with 0x00. To recover the var variable, we initialise it with 0x01.
             if (var == 0x00)
             {
                 var = 0x01;
             }
             
             ESP_ERROR_CHECK(set_bar_graph(var));
+            // var contains the data for the LEDs and it's been passed as an argument to set_bar_graph() function.
+            
             var = var << 1;
+            // It left shifts the values of var eg. from 0x01(means only the 8th led is on) it changes to 
+            // 0x02 (means only the 7th LED is on). Similarly, the LED's are turned on one by one from the 8th to the 1st.
             vTaskDelay(1000 / portTICK_PERIOD_MS);
         }
     }

c30604ef4d58f0c6791d1d0c6dab8b664c521b4a
chaitravi-ce <chaitravi007@gmail.com>
2021-02-15 18:47:29 +0530

    Documented switch_control_motor_parallel

--- 5_switch_controlled_motor_parallel/main/switch_control_motor_parallel.c
+++ 5_switch_controlled_motor_parallel/main/switch_control_motor_parallel.c
@@ -1,6 +1,6 @@
 #include "sra_board.h"
 
-static const char* TAG_SWITCH_CONTROL_MOTOR = "switch_control";
+static const char* TAG_SWITCH_CONTROL_MOTOR = "switch_control";			
 
 void drive_task(void *arg)
 {
@@ -10,19 +10,19 @@ void drive_task(void *arg)
 
 	while(1)
 	{
-		if(read_switch(SWITCH_1))
+		if(read_switch(SWITCH_1))   					                //Reading the input of switch 1
 		{
-			set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, 80);
-			ESP_LOGI(TAG_SWITCH_CONTROL_MOTOR, "MOTOR_A_0 FORWARD");	
+			set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, 80);		        //Setting the speed of motor A in forward direction, duty cycle 80%
+			ESP_LOGI(TAG_SWITCH_CONTROL_MOTOR, "MOTOR_A_0 FORWARD");    //Sending a message to esp log file	
 		}
-		else if(read_switch(SWITCH_2))
+		else if(read_switch(SWITCH_2))                                  //Reading the input of switch 2
 		{
-			set_motor_speed(MOTOR_A_0, MOTOR_BACKWARD, 80);
+			set_motor_speed(MOTOR_A_0, MOTOR_BACKWARD, 80);             //Setting the speed of motor A in reverse direction, duty cycle 80%
 			ESP_LOGI(TAG_SWITCH_CONTROL_MOTOR, "MOTOR_A_0 BACKWARD");
 		}
 		else
 		{
-			set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
+			set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);                  //If both switches have zero input, stop.
 		}
 
 		if(read_switch(SWITCH_3))
@@ -39,8 +39,7 @@ void drive_task(void *arg)
 		{
 			set_motor_speed(MOTOR_B_0, MOTOR_STOP, 0);
 		}
-
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    vTaskDelay(100 / portTICK_PERIOD_MS);					//Delays the task by 1 Tick-Period
 	}
 }
 
@@ -49,7 +48,12 @@ void app_main()
 {
 	 /**
      * @brief Basic Function for task creation
-     * 
      */
-    xTaskCreate(&drive_task,"drive_task",4096,NULL,1,NULL);
-}
+	//Task is created and added to the lists of tasks ready to run
+    xTaskCreate(&drive_task,					//address of the function to execute
+		"drive_task",							//name of the task
+		4096,								    //Memory allocated to the Task ie. 4096(4 bytes)
+		NULL,								    //Paramter passed as input(if any)
+		1,								        //Priority of the task
+		NULL);                       			//Task handle
+}                                                                                 

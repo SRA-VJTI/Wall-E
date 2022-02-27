65265c2fe7b2adb2ba6ce0d4b5072560eb226f14
gautam-dev-maker <gautamnagrawal@gmail.com>
2022-02-27 06:57:48 +0530

    Add PID Graphs to 8_self_balancing

--- 8_self_balancing/main/include/tuning_websocket_server.h
+++ 8_self_balancing/main/include/tuning_websocket_server.h
@@ -1,5 +1,5 @@
-#ifndef TUNING_HTTP_SERVER_H
-#define TUNING_HTTP_SERVER_H
+#ifndef TUNNING_WEBSOCKEt_SERVER_H
+#define TUNNING_WEBSOCKEt_SERVER_H
 
 #include <string.h>
 #include <fcntl.h>
@@ -19,6 +19,7 @@
 #include "cJSON.h"
 #include "sdkconfig.h"
 #include "wifi_handler.h"
+#include "websocket_server.h"
 
 #define MDNS_INSTANCE "walle pid tuning web server"
 #define MDNS_HOST_NAME CONFIG_MDNS_HOST_NAME
@@ -26,6 +27,7 @@
 #define FILE_PATH_MAX (ESP_VFS_PATH_MAX + 128)
 #define SCRATCH_BUFSIZE (10240)
 #define CHECK_FILE_EXTENSION(filename, ext) (strcasecmp(&filename[strlen(filename) - strlen(ext)], ext) == 0)
+
 
 typedef struct pid_const
 {
@@ -37,6 +39,7 @@ typedef struct pid_const
 } pid_const_t;
 
 pid_const_t read_pid_const();
-void start_tuning_http_server();
+void start_websocket_server();
+void plot_graph(float p_term, float d_term, float i_term);
 
 #endif

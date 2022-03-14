4d9971dfb3b67f90f0e870a5aeb77ab27c86aeaa
himanshu-02 <himanshuchougule3430@gmail.com>
2022-03-14 15:37:10 +0530

    create single script for mac and linux

--- wall_e_install.sh
+++ wall_e_install.sh
@@ -1,18 +1,42 @@
 #!/usr/bin/env bash
 
+echo "installing ESP IDF"
+
+_shell_="${SHELL#${SHELL%/*}/}"
+
+# Check whether esp-idf has already been installed
 if [ -d $HOME/esp/esp-idf ]; then
     echo "You have installed esp-idf" && exit 0
 fi
 
-echo "Installing ESP IDF"
-sudo apt update && sudo apt upgrade -y
-sudo apt install git wget flex bison gperf python3 python3-pip python3-setuptools cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0 -y
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+        sudo apt update && sudo apt upgrade -y
+        sudo apt install git wget flex bison gperf python3 python3-pip python3-setuptools cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0 -y
+        ;;
+    Darwin*)
+        if brew --version | grep -q 'Homebrew'; then
+            echo "Homebrew is already installed"
+        else 
+            echo "installing homebrew"
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            echo "homebrew installed successfully"
+        fi
+        brew install git cmake ninja dfu-util python3 pip 
+       ;;
+    *)          echo "Not supported :${unameOut}"
+esac
+
 mkdir -p "$HOME/esp"
 pushd "$HOME"/esp || (echo "Error: Cannot Make Directory" && exit 1)
 
 #ESP Toolchain & ESP-IDF
 git clone -b release/v4.2 --recursive https://github.com/espressif/esp-idf.git
-cd "$HOME"/esp/esp-idf
+pushd esp-idf
 ./install.sh
-echo "alias get_idf='. \$HOME/esp/esp-idf/export.sh'" >> ~/.bashrc
-source ~/.bashrc
+popd
+popd
+
+echo "alias get_idf='. $HOME/esp/esp-idf/export.sh'" >> $HOME/"$_shell_".rc
+source "$HOME"/"$_shell_".rc

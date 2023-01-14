1cb0f8751bd6c46b5284759e2389184738c451cb
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2023-01-15 03:47:57 +0530

    Update 7_line_following wrt to latest sra-board-component
    * Update OLED APIs configuration
    * Update LSA configuration to include 5th sensor
    * Change the LSA weights to {-5,-3,1,3,5}
    * Declare BLACK_BOUNDARY macro to distinguish between
      black and white lines

--- 7_line_following/main/line_following.c
+++ 7_line_following/main/line_following.c
@@ -5,15 +5,16 @@
 #include "tuning_http_server.h"
 
 #define MODE NORMAL_MODE
-#define BLACK_MARGIN 400
-#define WHITE_MARGIN 2000
+#define BLACK_MARGIN 4095
+#define WHITE_MARGIN 0
 #define bound_LSA_LOW 0
 #define bound_LSA_HIGH 1000
+#define BLACK_BOUNDARY  700    // Boundary value to distinguish between black and white readings
 
 /*
  * weights given to respective line sensor
  */
-const int weights[4] = {3,1,-1,-3};
+const int weights[5] = {-5, -3, 1, 3, 5};
 
 /*
  * Motor value boundts
@@ -38,9 +39,9 @@ void lsa_to_bar()
 {   
     uint8_t var = 0x00;                     
     bool number[8] = {0,0,0,0,0,0,0,0};
-    for(int i = 0; i < 4; i++)
+    for(int i = 0; i < 5; i++)
     {
-        number[7-i] = (line_sensor_readings.adc_reading[i] < BLACK_MARGIN) ? 0 : 1; //If adc value is less than black margin, then set that bit to 0 otherwise 1. 
+        number[7-i] = (line_sensor_readings.adc_reading[i] < BLACK_BOUNDARY) ? 0 : 1; //If adc value is less than black margin, then set that bit to 0 otherwise 1.
         var = bool_to_uint8(number);  //A helper function to convert bool array to unsigned int.
         ESP_ERROR_CHECK(set_bar_graph(var)); //Setting bar graph led with unsigned int value.
     }
@@ -62,21 +63,29 @@ void calculate_error()
 {
     int all_black_flag = 1; // assuming initially all black condition
     float weighted_sum = 0, sum = 0; 
-    float pos = 0;
-    
-    for(int i = 0; i < 4; i++)
+    float pos = 0; int k = 0;
+
+    for(int i = 0; i < 5; i++)
     {
-        if(line_sensor_readings.adc_reading[i] > BLACK_MARGIN)
+        if(line_sensor_readings.adc_reading[i] > BLACK_BOUNDARY)
         {
             all_black_flag = 0;
         }
-        weighted_sum += (float)(weights[i]) * (line_sensor_readings.adc_reading[i]);
-        sum = sum + line_sensor_readings.adc_reading[i];
+        if(line_sensor_readings.adc_reading[i] > 700)
+        {
+            k = 1;
+        }
+        if(line_sensor_readings.adc_reading[i] < 700)
+        {
+            k = 0;
+        }
+        weighted_sum += (float)(weights[i]) * k;
+        sum = sum + k;
     }
 
     if(sum != 0) // sum can never be 0 but just for safety purposes
     {
-        pos = weighted_sum / sum; // This will give us the position wrt line. if +ve then bot is facing left and if -ve the bot is facing to right.
+        pos = (weighted_sum - 1) / sum; // This will give us the position wrt line. if +ve then bot is facing left and if -ve the bot is facing to right.
     }
 
     if(all_black_flag == 1)  // If all black then we check for previous error to assign current error.
@@ -102,40 +111,43 @@ void line_follow_task(void* arg)
     ESP_ERROR_CHECK(enable_line_sensor());
     ESP_ERROR_CHECK(enable_bar_graph());
 #ifdef CONFIG_ENABLE_OLED
-    // Declaring the required OLED struct
-    u8g2_t oled_config;
-
     // Initialising the OLED
-    ESP_ERROR_CHECK(init_oled(&oled_config));
+    ESP_ERROR_CHECK(init_oled());
+    vTaskDelay(100);
+
+    // Clearing the screen
+    lv_obj_clean(lv_scr_act());
+
 #endif
-    
+
     while(true)
     {
         line_sensor_readings = read_line_sensor();
-        for(int i = 0; i < 4; i++)
+        for(int i = 0; i < 5; i++)
         {
-            line_sensor_readings.adc_reading[i] = bound(line_sensor_readings.adc_reading[i], BLACK_MARGIN, WHITE_MARGIN);
-            line_sensor_readings.adc_reading[i] = map(line_sensor_readings.adc_reading[i], BLACK_MARGIN, WHITE_MARGIN, bound_LSA_LOW, bound_LSA_HIGH);
+            line_sensor_readings.adc_reading[i] = bound(line_sensor_readings.adc_reading[i], WHITE_MARGIN, BLACK_MARGIN);
+            line_sensor_readings.adc_reading[i] = map(line_sensor_readings.adc_reading[i], WHITE_MARGIN, BLACK_MARGIN, bound_LSA_LOW, bound_LSA_HIGH);
+            line_sensor_readings.adc_reading[i] = 1000 - (line_sensor_readings.adc_reading[i]);
         }
-        
+
         calculate_error();
         calculate_correction();
         lsa_to_bar();
-        
-        left_duty_cycle = bound((optimum_duty_cycle - correction), lower_duty_cycle, higher_duty_cycle);
-        right_duty_cycle = bound((optimum_duty_cycle + correction), lower_duty_cycle, higher_duty_cycle);
+
+        left_duty_cycle = bound((optimum_duty_cycle + correction), lower_duty_cycle, higher_duty_cycle);
+        right_duty_cycle = bound((optimum_duty_cycle - correction), lower_duty_cycle, higher_duty_cycle);
 
         set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, left_duty_cycle);
         set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, right_duty_cycle);
 
-        
+
         //ESP_LOGI("debug","left_duty_cycle:  %f    ::  right_duty_cycle :  %f  :: error :  %f  correction  :  %f  \n",left_duty_cycle, right_duty_cycle, error, correction);
         ESP_LOGI("debug", "KP: %f ::  KI: %f  :: KD: %f", read_pid_const().kp, read_pid_const().ki, read_pid_const().kd);
 #ifdef CONFIG_ENABLE_OLED
         // Diplaying kp, ki, kd values on OLED 
         if (read_pid_const().val_changed)
         {
-            display_pid_values(read_pid_const().kp, read_pid_const().ki, read_pid_const().kd, &oled_config);
+            display_pid_values(read_pid_const().kp, read_pid_const().ki, read_pid_const().kd);
             reset_val_changed_pid_const();
         }
 #endif
